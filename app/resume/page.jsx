ee9bc43c61ce967b6a819573c456fa31281bfd91
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-06-02 03:03:18 +0600

    Skill Content & its element added . Make it responsive also

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -222,8 +222,29 @@ import { Description, Title } from "@radix-ui/react-dialog";
             </TabsContent>
             
              {/* skill */}
-             <TabsContent value="skills" className="w-full h-full">
-              Skills
+             <TabsContent value="skills" className="w-full h-full ">
+              <div className='flex flex-col gap-[30]'>
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.Title}</h3>
+                  <p className="max-w-[600px] text-white/60 max-auto xl:mx-0">{skills.Description}</p>
+                </div>
+                <ul className="grid grid-cols-2 mt-5 sm:grid-cols-3 md:grid-cols-4 gap-4 justify-center items-center xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return <li key={index}>
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full h-[150px]  bg-[#232329] rounded-xl transition-all duration-500  flex justify-center items-center group">
+                            <div className="text-6xl group-hover:text-accent transition-all duration-300">{skill.icon}</div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{skill.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  })}
+                </ul>
+              </div>
             </TabsContent>
             
              {/* about */}
